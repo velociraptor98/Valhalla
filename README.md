d80d4c5e0e2f2058cc7b1100b246a31de6d1518c
Kunal Singh <kunal@smartserv.io>
2022-08-15 20:19:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Valhalla
+
+Developed with Unreal Engine 5
---
+++ Source/Valhalla/ValhallaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ValhallaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALHALLA_API AValhallaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
